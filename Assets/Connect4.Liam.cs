fd739d445e696e83e4a61a6e115e59e09346b1a4
Liam <ltconflans@gmail.com>
2026-03-03 16:43:56 +0100

    IA bof

---
+++ Assets/Connect4.Liam.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public partial class Connect4 : MonoBehaviour
+{
+    
+ float Eval_Liam_Taccon(CellType[,] Board, CellType joueur, int colonne)
+    {
+        float score = 0.0f;
+        Coords test = DropToken(Board,colonne);
+        
+        if (TestIfWon(Board, joueur, test))
+        {
+            score = 100000;
+        }
+
+        if (TestIfWon(Board, CellType.Player1, test))
+        {
+            score = 10000;
+        }
+
+        if (!TestToken(joueur, (short)colonne))
+        {
+            score = -100000;
+        }
+
+        if (1 < test.X && test.X < 5)
+        {
+            score += 1;
+        }
+
+        if (test.Y >= 4)
+        {
+            score -= 3;
+        }
+
+        score += CompteSuite(test, joueur);
+        
+        
+        Debug.Log("score : " + score);
+        return score;
+
+    }
+
+    private int CompteSuite(Coords test, CellType joueur)
+    {
+        int suite = 0;
+        bool autreCote = false;
+        int directionX = 0;
+        int directionY = 0;
+
+        for (int j = 0; j < 4; j++)
+        {
+            switch (j)
+            {
+                case (0):
+                    directionX = -1;
+                    directionY = 0;
+                    break;
+                case (1):
+                    directionX = 0;
+                    directionY = 1;
+                    break;
+                case (2):
+                    directionX = 1;
+                    directionY = 1;
+                    break;
+                case (3):
+                    directionX = 1;
+                    directionY = -1;
+                    break;
+
+            }
+            autreCote = false;
+            for (int i = 1; i < 4; i++)
+            {
+                if (!autreCote)
+                {
+                    if (!(test.X + i * directionX < 0 || test.X + i * directionX > 5 ||
+                          test.Y + i * directionY > 6 || test.Y + i * directionY < 0))
+                    {
+                        if (Board[test.X + i * directionX, test.Y + i * directionY] != joueur)
+                        {
+                            autreCote = true;
+                            i = 1;
+                        }
+                        else
+                        {
+                            suite += 2;
+                        }
+                    }
+                    else
+                    {
+                        autreCote = true;
+                        i = 1;
+                    }
+                }
+
+                if (autreCote)
+                {
+                    if (!(test.X - i * directionX < 0 || test.X - i * directionX > 5 ||
+                          test.Y - i * directionY > 6 || test.Y - i * directionY < 0))
+                    {
+                        if (Board[test.X - i * directionX, test.Y - i * directionY] != joueur)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            suite += 2;
+                        }
+                    }
+                }
+            }
+        }
+
+        return suite;
+    }
+}
+
--- Assets/Connect4.cs
+++ Assets/Connect4.cs
@@ -1,10 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public partial class Connect4 : MonoBehaviour
 {
     public enum CellType
     {
@@ -14,15 +14,15 @@ public class GameManager : MonoBehaviour
     }
 
 
-    
+
     private CellType[,] Board = new CellType[6, 7];
     private string ez;
     private bool player1Turn = true;
 
     [SerializeField] private GameObject endScreen;
-    
-    [Header("images")]
-    [SerializeField] private Image[] caseLigne0;
+    [SerializeField] private TMP_Text endScreennText;
+
+    [Header("images")] [SerializeField] private Image[] caseLigne0;
     [SerializeField] private Image[] caseLigne1;
     [SerializeField] private Image[] caseLigne2;
     [SerializeField] private Image[] caseLigne3;
@@ -34,7 +34,7 @@ public class GameManager : MonoBehaviour
     private Stack<CellType> undoPlayer = new Stack<CellType>();
     private Stack<Coords> redoCoords = new Stack<Coords>();
     private Stack<CellType> redoPlayer = new Stack<CellType>();
-    
+
     private void Start()
     {
         tableau[0] = caseLigne0;
@@ -44,43 +44,68 @@ public class GameManager : MonoBehaviour
         tableau[4] = caseLigne4;
         tableau[5] = caseLigne5;
         DisplayBoard(Board);
-        
+
     }
 
     public void Button(int colonne)
     {
-        
-        
-        if (player1Turn && TestToken(CellType.Player1,(short)colonne))
+        redoCoords.Clear();
+        redoPlayer.Clear();
+
+        if (player1Turn && TestToken(CellType.Player1, (short)colonne))
         {
-            Coords toto = DropToken(Board, CellType.Player1, colonne);
+            Coords toto = DropToken(Board, colonne);
             Board[toto.X, toto.Y] = CellType.Player1;
             DisplayBoard(Board);
             TestIfWon(Board, CellType.Player1, toto);
-            
+
             undoCoords.Push((toto));
             undoPlayer.Push(CellType.Player1);
-        }
-        else if (!player1Turn && TestToken(CellType.Player2,(short)colonne))
-        {
-            Coords toto2 = DropToken(Board, CellType.Player2, (int)Eval_Liam_Taccon(Board, CellType.Player2));
-            Board[toto2.X, toto2.Y] = CellType.Player2;
-            DisplayBoard(Board);
-            TestIfWon(Board, CellType.Player2, toto2);
-            
-            undoCoords.Push((toto2));
-            undoPlayer.Push(CellType.Player2);
+
+            if (TestIfWon(Board, CellType.Player1, toto))
+            {
+                endScreen.SetActive(true);
+                endScreennText.text = "joueur 1 gagnééééééééééé";
+                Debug.Log("joueur 1 gagnééééééééééé");
+            }
         }
         else
         {
             return;
         }
-        
-        TestIfFinished(Board);
-        player1Turn = !player1Turn;
 
+        PlayIA();
     }
-    
+
+    private void PlayIA()
+    {
+        int bestMove = 0;
+        float bestMoveScore = 0.0f;
+
+        for (int i = 0; i < Board.GetLength(1) - 1; i++)
+        {
+            if (Eval_Liam_Taccon(Board, CellType.Player2, i) > bestMoveScore)
+            {
+                bestMoveScore = Eval_Liam_Taccon(Board, CellType.Player2, i);
+                bestMove = i;
+            }
+
+        }
+
+        Coords toto2 = DropToken(Board, bestMove);
+        Board[toto2.X, toto2.Y] = CellType.Player2;
+        DisplayBoard(Board);
+        if (TestIfWon(Board, CellType.Player2, toto2))
+        {
+            endScreen.SetActive(true);
+            endScreennText.text = "IA gagnééééééééééé";
+            Debug.Log("joueur 2 gagnééééééééééé");
+        }
+
+        undoCoords.Push((toto2));
+        undoPlayer.Push(CellType.Player2);
+    }
+
     public struct Coords
     {
         public int X;
@@ -99,36 +124,37 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < Board.GetLength(1); j++)
             {
-                Board[i,j] = CellType.Empty;
+                Board[i, j] = CellType.Empty;
             }
-        } 
+        }
     }
-    
+
     void DisplayBoard(CellType[,] Board)
     {
         ez = "";
-        for (int i = Board.GetLength(0)-1; i >= 0 ; i--)
+        for (int i = Board.GetLength(0) - 1; i >= 0; i--)
         {
             for (int j = 0; j < Board.GetLength(1); j++)
             {
-                switch (Board[i,j])
+                switch (Board[i, j])
                 {
-                    case(CellType.Empty):
+                    case (CellType.Empty):
                         ez += "[   ]";
                         tableau[i][j].GetComponent<Image>().color = Color.white;
                         break;
-                    case(CellType.Player1):
+                    case (CellType.Player1):
                         ez += "[X]";
                         tableau[i][j].GetComponent<Image>().color = Color.red;
                         break;
-                    case(CellType.Player2):
+                    case (CellType.Player2):
                         ez += "[O]";
                         tableau[i][j].GetComponent<Image>().color = Color.yellow;
                         break;
                 }
             }
+
             ez += "\n";
-        } 
+        }
         //.Log(ez);
     }
 
@@ -137,41 +163,44 @@ public class GameManager : MonoBehaviour
 
     bool TestToken(CellType joueur, short colonne)
     {
-        if (Board[Board.GetLength(0)-1, colonne] == CellType.Empty)
+        if (Board[Board.GetLength(0) - 1, colonne] == CellType.Empty)
         {
             return true;
         }
 
         return false;
     }
-    
-    private Coords DropToken(CellType[,] Board, CellType joueur, int colonne)
+
+    private Coords DropToken(CellType[,] Board, int colonne)
     {
         for (int i = 0; i < Board.GetLength(1) - 1; i++)
         {
             if (Board[i, colonne] == CellType.Empty)
             {
-                return new Coords(i,colonne);
+                return new Coords(i, colonne);
             }
         }
+
         return new Coords(-1, -1);
     }
 
     bool TestIfFinished(CellType[,] Board)
     {
-        for (int i = 0; i < Board.GetLength(1)-1; i++)
+        for (int i = 0; i < Board.GetLength(1) - 1; i++)
         {
             if (Board[Board.GetLength(0) - 1, i] != CellType.Empty)
-            { 
+            {
                 return false;
             }
         }
+
+        Debug.Log("prooooooooooooooooooooooooooooooooooooooooooooooooouut");
         return true;
     }
 
-    
-    
-    
+
+
+
     bool TestIfWon(CellType[,] Board, CellType joueur, Coords coupJoue)
     {
         int suite = 1;
@@ -180,51 +209,52 @@ public class GameManager : MonoBehaviour
         int directionX = 0;
         int directionY = 0;
 
-        Debug.Log("coupJoue  "+coupJoue.X+"/"+coupJoue.Y);
+        Debug.Log("coupJoue  " + coupJoue.X + "/" + coupJoue.Y);
 
         for (int j = 0; j < 4; j++)
         {
             switch (j)
             {
-                case(0): //vertical
+                case (0): //vertical
                     Debug.Log("vertical");
                     directionX = -1;
                     directionY = 0;
                     break;
-                case(1): //horizontal
+                case (1): //horizontal
                     Debug.Log("horizontal");
                     directionX = 0;
                     directionY = 1;
                     break;
-                case(2): // diago 
+                case (2): // diago 
                     Debug.Log("premiere diago");
                     directionX = 1;
                     directionY = 1;
                     break;
-                case(3):
+                case (3):
                     Debug.Log("deuxieme diago");
                     directionX = 1;
                     directionY = -1;
                     break;
-                
+
             }
 
             suite = 1;
             autreCote = false;
-            
+
             for (int i = 1; i < 4; i++)
             {
                 if (!autreCote)
                 {
-                    
+
                     //Debug.Log((coupJoue.X + i * directionX) + " " + (coupJoue.Y + i * directionY));
-                    if (!(coupJoue.X + i * directionX < 0 || coupJoue.X + i * directionX > 5 || coupJoue.Y + i * directionY > 6 || coupJoue.Y + i * directionY < 0))
+                    if (!(coupJoue.X + i * directionX < 0 || coupJoue.X + i * directionX > 5 ||
+                          coupJoue.Y + i * directionY > 6 || coupJoue.Y + i * directionY < 0))
                     {
-                        
+
                         //Debug.Log("kjhkjhgkjhg  "+(coupJoue.X + i * directionX)+"  "+ (coupJoue.Y + i * directionY));
                         if (Board[coupJoue.X + i * directionX, coupJoue.Y + i * directionY] != joueur)
                         {
-                            
+
                             Debug.Log("changement de coté");
                             autreCote = true;
                             i = 1;
@@ -233,7 +263,9 @@ public class GameManager : MonoBehaviour
                         {
                             suite++;
                             //Debug.Log("dirx= "+directionX + "diry= "+directionY);
-                            Debug.Log("suite : "+ suite + "    Coords : " + +(coupJoue.X + i * directionX)+"/"+ (coupJoue.Y + i * directionY) + "   dirX : " + directionX +" dirY : " + directionY);
+                            Debug.Log("suite : " + suite + "    Coords : " + +(coupJoue.X + i * directionX) + "/" +
+                                      (coupJoue.Y + i * directionY) + "   dirX : " + directionX + " dirY : " +
+                                      directionY);
                         }
                     }
                     else
@@ -248,7 +280,8 @@ public class GameManager : MonoBehaviour
                 {
 
                     //Debug.Log((coupJoue.X - i*directionX) + " " + (coupJoue.Y - i*directionY));
-                    if (!(coupJoue.X - i * directionX < 0 || coupJoue.X - i * directionX > 5 || coupJoue.Y - i * directionY > 6 || coupJoue.Y - i * directionY < 0))
+                    if (!(coupJoue.X - i * directionX < 0 || coupJoue.X - i * directionX > 5 ||
+                          coupJoue.Y - i * directionY > 6 || coupJoue.Y - i * directionY < 0))
                     {
                         //Debug.Log("noinonoion  "+(coupJoue.X - i * directionX)+"  "+ (coupJoue.Y - i * directionY));
                         if (Board[coupJoue.X - i * directionX, coupJoue.Y - i * directionY] != joueur)
@@ -259,7 +292,9 @@ public class GameManager : MonoBehaviour
                         else
                         {
                             suite++;
-                            Debug.Log("suite : "+suite + "    Coords : " + +(coupJoue.X - i * directionX)+"/"+ (coupJoue.Y - i * directionY) + "   dirX : " + directionX +" dirY : " + directionY);
+                            Debug.Log("suite : " + suite + "    Coords : " + +(coupJoue.X - i * directionX) + "/" +
+                                      (coupJoue.Y - i * directionY) + "   dirX : " + directionX + " dirY : " +
+                                      directionY);
                         }
                     }
                     else
@@ -270,48 +305,43 @@ public class GameManager : MonoBehaviour
 
                 if (suite >= 4)
                 {
-                    endScreen.SetActive(true);
-                    Debug.Log("gagnééééééééééé");
                     return true;
                 }
             }
         }
+
         return false;
     }
-    
+
 
     public void Reset()
     {
         SceneManager.LoadScene(0);
     }
-    
+
     public void Undo()
     {
-        if(undoCoords.Count == 0)return;
-        
+        if (undoCoords.Count == 0) return;
+
         Board[undoCoords.Peek().X, undoCoords.Peek().Y] = CellType.Empty;
         redoCoords.Push(undoCoords.Pop());
         redoPlayer.Push(undoPlayer.Pop());
-        
+
         DisplayBoard(Board);
     }
-    
+
 
     public void Redo()
     {
-        if(redoCoords.Count == 0)return;
-        
+        if (redoCoords.Count == 0) return;
+
         Board[redoCoords.Peek().X, redoCoords.Peek().Y] = redoPlayer.Peek();
         undoCoords.Push(redoCoords.Pop());
         undoPlayer.Push(redoPlayer.Pop());
-        
+
         DisplayBoard(Board);
     }
-    
-    
-    float Eval_Liam_Taccon(CellType[,] Board, CellType joueur)
-    {
-        
-        return Random.Range(0, 7);
-    }
-}    
+}
+
+   
+
